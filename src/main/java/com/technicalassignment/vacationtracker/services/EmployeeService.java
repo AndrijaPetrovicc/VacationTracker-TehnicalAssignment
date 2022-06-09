d8026cf942df9ac99c1a6ef4eafb2b73afcd3e8e
andrijapetrovic <andrija1304198@gmail.com>
2022-06-09 12:54:49 +0200

    I added the EmployeeService class and in it methods saveEmployeeProfiles and getEmployeeIdByEmail

---
+++ src/main/java/com/technicalassignment/vacationtracker/services/EmployeeService.java
@@ -0,0 +1,30 @@
+package com.technicalassignment.vacationtracker.services;
+
+import com.technicalassignment.vacationtracker.helpers.CSVHelper;
+import com.technicalassignment.vacationtracker.models.Employee;
+import com.technicalassignment.vacationtracker.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+//The Service annotation tells Spring that this is a service class
+@Service
+public class EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    //The saveEmployeeProfiles method receives MultipartFile as a parameter and uses the csvToEmployeeProfiles method and the saveAll method to store the relevant data in the database
+    public void saveEmployeeProfiles(MultipartFile file){
+        try{
+            List<Employee> employees = CSVHelper.csvToEmployeeProfiles(file.getInputStream());
+            employeeRepository.saveAll(employees);
+        } catch (IOException e){
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+    //getEmployeeIdByEmail method as a parameter receives a string that should represent the email address of the employee and based on that address returns the employee ID
+    public Long getEmployeeIdByEmail(String email){
+        return employeeRepository.findByEmail(email).getId();
+    }
+}
